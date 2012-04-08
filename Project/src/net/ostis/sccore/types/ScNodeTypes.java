effeb17a3bd598e6da6d8fa7f7e966b84ba18d6a
Vasya Posudevski <NM Group@NMGroup-.(none)>
2012-04-08 20:18:26 +0300

    delete (ScNodeTypes.java)

--- Project/src/net/ostis/sccore/types/ScNodeTypes.java
+++
@@ -1,9 +0,0 @@
-package net.ostis.sccore.types;
-
-/**
- * Class that contain types of sc node.
- * @author yaskoam
- */
-public class ScNodeTypes {
-    public static final String CONST = "CONST";
-}
