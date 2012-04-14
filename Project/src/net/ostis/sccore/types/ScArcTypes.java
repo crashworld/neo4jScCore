fb10fed49cbe93e6a3f97de2a21da1dde97691bf
Anton Sidorovich <cidyuk@gmail.com>
2012-04-15 01:11:28 +0200

    Adding and getting types to/from arc and node fixed. 3_*_*_* iterators ready to use)

--- Project/src/net/ostis/sccore/types/ScArcTypes.java
+++
@@ -1,9 +0,0 @@
-package net.ostis.sccore.types;
-
-/**
- * Class that contain different types of sc arcs.
- * @author yaskoam
- */
-public class ScArcTypes {
-    public static final String CONST = "CONST";
-}
